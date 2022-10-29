4348d126b57b459a71b5f9b0b04f88bfcf70e824
ahmed-mostaf <113672935+ahmed-mostaf@users.noreply.github.com>
2022-10-29 21:14:30 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,3 +3,4 @@ Navbar and scroll to them when you click on them
 A section is highlighted in the viewport when scrolling */
 function Nav_Bar_buttons() {
 /*The following methods are used to get elements from an html
+document and create elements, text, and links for the navbar*/
