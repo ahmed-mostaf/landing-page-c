82a61ae5eff2903d65d736ed619e11e4f5453bf2
ahmed-mostaf <113672935+ahmed-mostaf@users.noreply.github.com>
2022-11-14 21:39:18 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,3 +9,4 @@ let all_part = document.getElementsByTagName("section");
 let ul_nav = document.getElementById("navbar__list");
 for (let section of all_part) {
   let relate = document.createElement("li");
+  let for_connect = document.createElement("a");
